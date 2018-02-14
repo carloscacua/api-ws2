c1b4e407d18e98c623cb8c6651932123d3e9b562
Carlos Castillo <carloscastillo@Carloss-MacBook-Pro.local>
2018-02-13 23:45:50 -0400

    OAuth2 implementation and loggers: console and file

--- src/main/java/ws/config/InterceptorConfig.java
+++ src/main/java/ws/config/InterceptorConfig.java
@@ -1,19 +1,25 @@
 package ws.config;
 
 import ws.interceptors.HeaderValidatorInterceptor;
+import ws.interceptors.LoggerInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class ValidatorConfig extends WebMvcConfigurerAdapter {
+public class InterceptorConfig extends WebMvcConfigurerAdapter {
 
     @Autowired
     private HeaderValidatorInterceptor headerValidator;
 
+    @Autowired
+    private LoggerInterceptor loggerInterceptor;
+
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(headerValidator).addPathPatterns("/header/**");
+        registry.addInterceptor(loggerInterceptor);
     }
 }
---
+++ src/main/java/ws/constants/GlobalConstants.java
@@ -0,0 +1,16 @@
+package ws.constants;
+
+public class GlobalConstants {
+    public final static String AUTHORIZATION = "Authorization";
+    public static final String ROLE_CLIENT = "ROLE_CLIENT";
+    public static final String WRITE_SCOPE = "write";
+    public static final String READ_SCOPE = "read";
+    public static final String BEARER = "Bearer";
+    public static final String EMPTY_STRING = "";
+    public static final String USERNAME = "charles";
+    public static final String CLIENT_NAME = "ios_rest_call";
+    public static final String PASSWORD = "123456";
+    public static final String CLIENT_CREDENTIALS = "credentials";
+    public static final String LOGGER_FILE_PATH = "audit_log.txt";
+}
+
